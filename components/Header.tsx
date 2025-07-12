5ddde098513d02f7efd4304a484893ae5cb36ea5
Janet Spellman <janetspellman13@gmail.com>
2025-07-11 19:05:21 -0700

    feat: heroui installed, working on header component

---
+++ components/Header.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@heroui/react";
+
+export const AcmeLogo = () => {
+  return (
+    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+      <path
+        clipRule="evenodd"
+        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const mobileNavMenuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href="https://www.heroui.com/docs/components/navbar#with-dropdown-menu"
+          >
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link
+            aria-current="page"
+            href="https://www.heroui.com/docs/components/navbar#with-dropdown-menu"
+          >
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href="https://www.heroui.com/docs/components/navbar#with-dropdown-menu"
+          >
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="https://www.heroui.com/docs/components/navbar#with-dropdown-menu">
+            Login
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      {/* ################  MOBILE #################*/}
+      <NavbarMenu>
+        {mobileNavMenuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2
+                  ? "primary"
+                  : index === mobileNavMenuItems.length - 1
+                    ? "danger"
+                    : "foreground"
+              }
+              href="https://www.heroui.com/docs/components/navbar#with-dropdown-menu"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
