3d7e9fe43aaabb812603bee2423a2dbfab67e59d
Janet Spellman <janetspellman13@gmail.com>
2025-07-12 01:26:36 -0700

    refactor: added accessiblity to a native button, added comments

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,56 +17,38 @@ import {
   DropdownMenu,
 } from "@heroui/react";
 
+// NavBarMenuToggle == toggles mobile nav bar
+// NavBarMenu == mobile nav bar
+
+import listOfServices from "@/data/services";
+
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const UserIsSignedIn = false;
+  const UserIsSignedIn = true;
 
   const handleLogOut = () => {
+    console.log("logged out :)");
     // logout Logic
   };
 
-  const staticMenuItems = [
-    {
-      label: "House Cleaning",
-      href: "/housecleaning",
-      description: "Regular, deep and move-in/out cleaning",
-    },
-    {
-      label: "Lawn & Garden",
-      href: "/lawnandgarden",
-      description: "Mowing, landscaping, yard maintenance",
-    },
-    {
-      label: "HandyMan & Repairs",
-      href: "/handymanandrepairs",
-      description: "Plumbing, electrical, installations, general fixes",
-    },
-    {
-      label: "Exterior Cleaning",
-      href: "/exteriorcleaning",
-      description: "Driveways, siding, decks, patios",
-    },
-    {
-      label: "Specialized Cleaning",
-      href: "/specializedcleaning",
-      description: "Carpet, upholstery, post-construction",
-    },
-    {
-      label: "Assembly & Installation",
-      href: "/assemblyandinstallation",
-      description: "Furniture assembly, TV mounting, applicances",
-    },
-  ];
+  const staticMenuItems = listOfServices;
 
-  const loggedInMenuItems = [
-    { label: "Profile", href: "/profile" },
+  type LoggedInMenuType =
+    | { label: string; href: string; onClick?: never }
+    | { label: string; onClick: () => void; href?: never };
+
+  // This is a union type. for objects you have to put the | at the start too, not just inbetween the two objects
+  // we either want an href or onClick property, never both
+
+  const loggedInMenuItems: LoggedInMenuType[] = [
     { label: "Dashboard", href: "/dashboard" },
-    { label: "login", onclick: handleLogOut },
+    { label: "Profile", href: "/profile" },
+    { label: "Log Out", onClick: handleLogOut },
   ];
 
   const guestMenuItems = [
-    { label: "login", onclick: "/login" },
-    { label: "sign Up", href: "/sign up" },
+    { label: "Sign Up", href: "/sign up" },
+    { label: "Login", href: "/login" },
   ];
 
   const mobileNavMenuItems = [
@@ -81,37 +63,40 @@ export default function App() {
         wrapper: "w-full max-w-none px-4", // gets rid of the default max-w-[1024px] set on the header by heroui
       }}
     >
+      {/* #### Left Section (logo and mobile menu toggle button) ###### */}
+
       <NavbarContent justify="start">
-        {/* Toggle for mobile menu */}
+        {/* Toggle for mobile menu, it hides on screens that are larger than extra small */}
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
-
-        {/* #### Logo Section ###### */}
-
         <NavbarBrand>
           <img
             src="/logo.png"
             alt="A corgi which is used as a logo"
             width={40}
+            height={50}
           />
-          <p className="font-bold text-inherit">Maid You Look</p>
+          <p className="text-medium font-bold">Maid You Look</p>
         </NavbarBrand>
       </NavbarContent>
 
+      {/* #### Middle Section ###### */}
       <NavbarContent className="hidden gap-4 sm:flex" justify="center">
         <Dropdown>
           <NavbarItem isActive>
             <DropdownTrigger>
-              <Button
-                disableRipple
-                className="bg-transparent p-0 data-[hover=true]:bg-transparent"
-                radius="sm"
-                variant="light"
+              {/* Used a regular button here since its just a non-stylized button. The stylized HeroUi button is overkill and would require workarounds like putting the services text in a span to make its sizing look the same as the other nav buttons */}
+              <button
+                className="p-0 font-medium"
+                type="button"
+                aria-haspopup="menu"
+                aria-expanded={isMenuOpen}
+                aria-controls="services-dropdown"
               >
                 Services
-              </Button>
+              </button>
             </DropdownTrigger>
           </NavbarItem>
 
@@ -133,16 +118,71 @@ export default function App() {
           </DropdownMenu>
         </Dropdown>
       </NavbarContent>
+
+      {/* #### Right Section ###### */}
       <NavbarContent justify="end">
-        {/* Buttons For guest users */}
-        <NavbarItem className="hidden lg:flex">
-          <Link href="/login">Log In</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="/signup" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
+        {!UserIsSignedIn && (
+          <>
+            <NavbarItem className="hidden md:flex">
+              {/* only shows up on medium and larger screens, when there is more room */}
+              <Link
+                className="w-full"
+                color="foreground"
+                href={guestMenuItems[0].href}
+                size="md"
+              >
+                {guestMenuItems[0].label}
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button
+                as={Link}
+                color="primary"
+                href={guestMenuItems[1].href}
+                variant="flat"
+              >
+                {guestMenuItems[1].label}
+              </Button>
+            </NavbarItem>
+          </>
+        )}
+
+        {UserIsSignedIn && (
+          <>
+            <NavbarItem className="hidden md:flex">
+              <Link
+                className="w-full"
+                color="foreground"
+                href={loggedInMenuItems[0].href}
+                size="md"
+              >
+                {loggedInMenuItems[0].label}
+              </Link>
+            </NavbarItem>
+
+            <NavbarItem className="hidden md:flex">
+              <Link
+                className="w-full"
+                color="foreground"
+                href={loggedInMenuItems[1].href}
+                size="md"
+              >
+                {loggedInMenuItems[1].label}
+              </Link>
+            </NavbarItem>
+
+            <NavbarItem>
+              <Button
+                color="primary"
+                onPress={loggedInMenuItems[2].onClick}
+                // onPress is HeroUi's version of onClick
+                variant="flat"
+              >
+                {loggedInMenuItems[2].label}
+              </Button>
+            </NavbarItem>
+          </>
+        )}
       </NavbarContent>
 
       {/* ################  Mobile Nav Bar Menu ################# */}
@@ -151,19 +191,25 @@ export default function App() {
         {mobileNavMenuItems.map((item, index) => (
           /* eslint-disable-next-line react/no-array-index-key */
           <NavbarMenuItem key={`${item.label}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                // the last item will be be red
-                index === mobileNavMenuItems.length - 1
-                  ? "danger"
-                  : "foreground"
-              }
-              href={item.href}
-              size="lg"
-            >
-              {item.label}
-            </Link>
+            {item.href ? (
+              <Link
+                className="w-full"
+                color="foreground"
+                href={item.href}
+                size="md"
+              >
+                {item.label}
+              </Link>
+            ) : (
+              "onClick" in item && (
+                // this typeguard check is needed because the item.href check returning false does not narrow the type down to being the second union member (item.onClick)
+
+                // the type of the item is actually the full union still, so typescript sees that it could be the first union type which doesn't have the onClick property
+                <button onClick={item.onClick} type="button">
+                  <span className="text-medium"> {item.label} </span>
+                </button>
+              )
+            )}
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
---
+++ data/services.ts
@@ -0,0 +1,33 @@
+export const services = [
+  {
+    label: "House Cleaning",
+    href: "/housecleaning",
+    description: "Regular, deep and move-in/out cleaning",
+  },
+  {
+    label: "Lawn & Garden",
+    href: "/lawnandgarden",
+    description: "Mowing, landscaping, yard maintenance",
+  },
+  {
+    label: "HandyMan & Repairs",
+    href: "/handymanandrepairs",
+    description: "Plumbing, electrical, installations, general fixes",
+  },
+  {
+    label: "Exterior Cleaning",
+    href: "/exteriorcleaning",
+    description: "Driveways, siding, decks, patios",
+  },
+  {
+    label: "Specialized Cleaning",
+    href: "/specializedcleaning",
+    description: "Carpet, upholstery, post-construction",
+  },
+  {
+    label: "Assembly & Installation",
+    href: "/assemblyandinstallation",
+    description: "Furniture assembly, TV mounting, applicances",
+  },
+];
+export default services;
