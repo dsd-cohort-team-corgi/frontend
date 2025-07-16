541c2cc9e05e001e55d0a25ead67f199270b90ee
Rafael Rios <rvrios22@gmail.com>
2025-07-16 06:18:11 -0700

    feat: service provider card and chips of services

--- app/providers/[slug]/page.tsx
+++ app/providers/[slug]/page.tsx
@@ -3,7 +3,17 @@ import Link from "next/link";
 import Image from "next/image";
 import leftArrow from "../../../public/leftArrow.svg";
 import searchIcon from "../../../public/searchIcon.svg";
-import { BreadcrumbItem, Breadcrumbs, Input } from "@heroui/react";
+import star from "../../../public/star.svg";
+import {
+  BreadcrumbItem,
+  Breadcrumbs,
+  Input,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Chip,
+} from "@heroui/react";
 import { lawnCareCompanies } from "@/data/mockProvidersList";
 import services from "../../../data/services";
 import { useState } from "react";
@@ -12,10 +22,21 @@ export default function Page({ params }: { params: { slug: string } }) {
   const [searchValue, setSearchValue] = useState<string>("Search Everything");
 
   //   returns an object from services array so we can have appropriate category header
-  const service = services.find(({ href }) => {
+  const categoryHeader = services.find(({ href }) => {
     const hrefWithNoSlash = href.split("/")[1];
     return hrefWithNoSlash === params.slug;
   });
+
+  const chipServicesSet = new Set();
+  const addServicesToChipServicesSet = () => {
+    lawnCareCompanies.forEach(({ services }) => {
+      services.forEach((service) => {
+        chipServicesSet.add(service);
+      });
+    });
+  };
+  addServicesToChipServicesSet();
+  const chipServicesArray = Array.from(chipServicesSet) as string[];
 
   return (
     <>
@@ -34,16 +55,46 @@ export default function Page({ params }: { params: { slug: string } }) {
         </BreadcrumbItem>
       </Breadcrumbs>
       <main>
-        <h1>{service?.label ? service.label : "Service Not Found"}</h1>
-        {/* Chips for sub services */}
+        <h1>
+          {categoryHeader?.label ? categoryHeader.label : "Service Not Found"}
+        </h1>
+        {/* Chip container */}
+        <div className="flex">
+          {chipServicesArray.map((service, idx) => (
+            <Chip key={idx}>{service}</Chip>
+          ))}
+        </div>
         <Input
           type="search"
           value={searchValue}
           endContent={
             <Image src={searchIcon} alt="Search Icon" height={25} width={25} />
           }
-          className="border-secondary-font-color mx-auto w-4/5 max-w-5xl rounded-xl border-1"
+          className="mx-auto w-4/5 max-w-5xl rounded-xl border-1 border-secondary-font-color"
         />
+        <p>{lawnCareCompanies.length} Providers Available</p>
+        {lawnCareCompanies.map(
+          ({ id, companyName, rating, numberOfReviews, services }) => (
+            <Card
+              key={id}
+              className="mx-auto mb-4 w-4/5 max-w-5xl border-1 border-secondary-font-color"
+            >
+              <CardHeader>{companyName}</CardHeader>
+              <CardBody className="flex flex-row items-center gap-1">
+                <Image src={star} alt="star icon" width={22} height={22} />
+                <span className="font-black">{rating}</span>
+                <span className="text-secondary-font-color">
+                  ({numberOfReviews} reviews)
+                </span>
+              </CardBody>
+              <CardFooter>
+                {services.map((service, idx) => (
+                  <Chip key={idx}>{service}</Chip>
+                ))}
+              </CardFooter>
+            </Card>
+          ),
+        )}
       </main>
     </>
   );
