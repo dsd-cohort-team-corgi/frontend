6e22eec3f0eb58c1f4aecc5cdda88e3d37b64f76
Janet Spellman <janetspellman13@gmail.com>
2025-07-21 04:54:39 -0700

    added styling

--- components/GoogleSignInbutton.tsx
+++ components/GoogleSignInbutton.tsx
@@ -63,6 +63,7 @@ export default function GoogleSignInButton() {
             size: "large",
             type: "standard",
             shape: "pill",
+            text: "continue_with",
           },
         );
       }
--- components/SignInModal.tsx
+++ components/SignInModal.tsx
@@ -25,34 +25,50 @@ type LoginPageType = {
 // https://www.npmjs.com/package/@react-oauth/google
 
 export default function LoginPage({ isOpen, onOpenChange }: LoginPageType) {
-  function googleSignIn() {
-    console.log("hi from google");
-  }
   return (
-    <Modal isOpen={isOpen} placement="top-center" onOpenChange={onOpenChange}>
+    <Modal
+      isOpen={isOpen}
+      placement="top-center"
+      onOpenChange={onOpenChange}
+      classNames={{
+        closeButton: "text-black top-8 right-6 hover:text-primary",
+        base: "max-w-[500px]",
+      }}
+    >
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1">
+            <ModalHeader className="my-4 flex flex-col gap-1 text-2xl font-extrabold">
               Sign in to continue{" "}
             </ModalHeader>
-            <ModalBody>
-              <h2> Complete Your Booking</h2>
-              <p>
-                <span>Service:</span> Lawn Mowing
-              </p>
-              <p>
-                <span>Date:</span> Monday, July ....{" "}
-              </p>
-              <p>
-                <span>Total:</span> $65
-              </p>
+            <ModalBody className="gap-0 pl-12">
+              <h2 className="mb-3 text-lg font-extrabold">
+                {" "}
+                Complete Your Booking
+              </h2>
+              <div>
+                <span className="font-semibold text-secondary-font-color">
+                  Service:
+                </span>{" "}
+                <span>Lawn Mowing</span>
+              </div>
+              <div>
+                <span className="font-semibold text-secondary-font-color">
+                  Date:
+                </span>
+                <span> Monday, July .... </span>
+              </div>
+              <div>
+                <span className="font-semibold text-secondary-font-color">
+                  Total:
+                </span>{" "}
+                <span className="font-bold"> $65</span>
+              </div>
             </ModalBody>
             <ModalFooter className="flex-col">
               {/* // footer automatically is flexed as flex-row, flex-none is ignored, so I told it to flex-col */}
-              <p className="w-full">
-                {" "}
-                Sign in or create an account to complete your booking{" "}
+              <p className="mb-3 ml-3 w-full">
+                Sign in or create an account to complete your booking
               </p>
               <GoogleSignInbutton />
             </ModalFooter>
