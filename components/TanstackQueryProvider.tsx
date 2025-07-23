b7499810e0ae6be770f227e80f388b489e4d967e
Rafael Rios <rvrios22@gmail.com>
2025-07-23 07:10:06 -0700

    feat: Tanstack query provider component and added to root layout

---
+++ components/TanstackQueryProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+// since this is a client component we need to pass children to it for our server components e.g. our root layout
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+function TanstackQueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default TanstackQueryProvider;
