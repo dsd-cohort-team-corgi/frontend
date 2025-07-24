26d0febf26fe2e925bc01e89a57103546c91efcf
Janet Spellman <janetspellman13@gmail.com>
2025-07-24 02:23:54 -0700

    bug, localstorage not reliably working in prod, simplifed code to have the redirect in the redirect string

--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -13,10 +13,7 @@ export default function GoogleSignInButton() {
     // so the window object is available, avoiding hydration errors
 
     if (typeof window === "undefined") return;
-
-    localStorage.setItem("redirectPath", window.location.pathname);
-    console.log("Origin:", window.location.origin);
-
+    const redirectPath = window.location.pathname;
     // lets make sure we're entirely signed out for a fresh log in
     // this will avoid errors with supabase accidently using an invalid old session for api calls
     // auth.signOut logs out of client
@@ -27,7 +24,7 @@ export default function GoogleSignInButton() {
       provider: "google",
 
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback?redirectPath=${redirectPath}`,
         queryParams: {
           prompt: "select_account",
         },
