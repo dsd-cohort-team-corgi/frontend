9af1b8973c9a9b3d883e873ebaa2b912ffeee67b
Rafael Rios <rvrios22@gmail.com>
2025-07-25 06:19:56 -0700

    add: files for booking modal

---
+++ components/BookingConfirmedModal.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Modal, useDisclosure } from "@heroui/react";
+function BookingConfirmedModal() {
+  return <div>BookingConfirmedModal</div>;
+}
+
+export default BookingConfirmedModal;
