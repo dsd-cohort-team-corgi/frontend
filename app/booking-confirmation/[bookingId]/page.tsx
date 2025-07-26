082169d2604c930229297841965e17eb4ca52629
Rafael Rios <rvrios22@gmail.com>
2025-07-26 12:37:26 -0700

    add: useApiQuery hook

--- app/booking-confirmation/[bookingId]/page.tsx
+++ app/booking-confirmation/[bookingId]/page.tsx
@@ -9,36 +9,43 @@ import {
   useDisclosure,
 } from "@heroui/react";
 import Check from "@/components/icons/Check";
+import { useApiQuery } from "@/lib/api-client";
 
 interface BookingModalProps {
   isOpen: boolean;
 }
 export default function page() {
-  const { isOpen } = useDisclosure();
+  const { isOpen, onOpen } = useDisclosure();
+  const { data, error, isLoading } = useApiQuery<unknown>(
+    ["bookings"],
+    "/bookings",
+  );
+
+  console.log(data)
   return (
     <>
-    <p>fasd</p>
-    <Modal
-      isOpen={isOpen}
-      placement="top-center"
-      classNames={{
-        closeButton: "invisible",
-        header: "flex-col justify-center items-center gap-4",
-        // the X defaults to the top right corner, moved it with top and right
-        base: "max-w-[500px]",
-        // default version was too small and had a max-w-m
-    }}
-    >
-      <ModalContent>
-        <ModalHeader className="text-center">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-green-200">
-            <Check color="#187a24" />
-          </div>
-          Booking Confirmed
-          <p className="text-light-font-color text-sm">ID: BK-001234</p>
-        </ModalHeader>
-      </ModalContent>
-    </Modal>
+      <p>fasd</p>
+      <Modal
+        isOpen={isOpen}
+        placement="top-center"
+        classNames={{
+          closeButton: "invisible",
+          header: "flex-col justify-center items-center gap-4",
+          // the X defaults to the top right corner, moved it with top and right
+          base: "max-w-[500px]",
+          // default version was too small and had a max-w-m
+        }}
+      >
+        <ModalContent>
+          <ModalHeader className="text-center">
+            <div className="flex h-10 w-10 items-center justify-center rounded-full bg-green-200">
+              <Check color="#187a24" />
+            </div>
+            Booking Confirmed
+            <p className="text-light-font-color text-sm">ID: BK-001234</p>
+          </ModalHeader>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,7 +1,7 @@
 import { useQuery, useMutation } from "@tanstack/react-query";
 import supabase from "./supabase";
 
-const API_BASE_URL = "https://maidyoulook-backend.onrender.com";
+const API_BASE_URL = "http://127.0.0.1:8000/";
 
 const getAuthHeaders = async () => {
   const headers: Record<string, string> = {
