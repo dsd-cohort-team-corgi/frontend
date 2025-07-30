04412fb1fb864b8613363523170ff629332e1d90
Janet Spellman <janetspellman13@gmail.com>
2025-07-29 23:11:58 -0700

    feat, stripe payment section styling

--- components/stripe/Checkout.tsx
+++ components/stripe/Checkout.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import { CircleAlert, Lock } from "lucide-react";
+import {
+  useStripe,
+  useElements,
+  CardNumberElement,
+  CardExpiryElement,
+  CardCvcElement,
+} from "@stripe/react-stripe-js";
 import convertToSubcurrency from "@/utils/stripe/convertToSubcurrency";
+import StyledAsButton from "../StyledAsButton";
 
 const CheckoutPage = function ({ amount }: { amount: number }) {
   const stripe = useStripe();
@@ -26,13 +34,16 @@ const CheckoutPage = function ({ amount }: { amount: number }) {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setLoading(true);
+    setErrorMessage(undefined);
 
     if (!stripe || !elements) {
+      setErrorMessage("Stripe.js has not loaded yet.");
+      setLoading(false);
       return;
     }
 
+    // Optional: submit elements to validate inputs before confirming
     const { error: submitError } = await elements.submit();
-
     if (submitError) {
       setErrorMessage(submitError.message);
       setLoading(false);
@@ -48,12 +59,7 @@ const CheckoutPage = function ({ amount }: { amount: number }) {
     });
 
     if (error) {
-      // This point is only reached if there's an immediate error when
-      // confirming the payment. Show the error to your customer (for example, payment details incomplete)
       setErrorMessage(error.message);
-    } else {
-      // The payment UI automatically closes with a success animation.
-      // Your customer is redirected to your `return_url`.
     }
 
     setLoading(false);
@@ -75,18 +81,59 @@ const CheckoutPage = function ({ amount }: { amount: number }) {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="rounded-md bg-white p-2">
-      {clientSecret && <CardElement />}
+    <form onSubmit={handleSubmit} className="mx-auto max-w-4xl rounded-md">
+      <section className="rounded-xl border-1 border-light-accent bg-white p-6">
+        <div className="my-auto flex max-w-4xl rounded-md bg-white py-4">
+          <Lock color="green" size={24} />
+          <h2 className="pl-2 text-xl font-semibold">Secure Payment </h2>
+        </div>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label className="mb-4 block">
+          <span>Card Number</span>
+          <div className="mt-1 rounded border border-gray-300 p-3">
+            <CardNumberElement options={{ showIcon: true }} />
+          </div>
+        </label>
 
-      {errorMessage && <div>{errorMessage}</div>}
+        <div className="mb-4 flex gap-4">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label className="flex-1">
+            <span> Expiry Date</span>
+            <div className="mt-1 rounded border border-gray-300 p-3">
+              <CardExpiryElement />
+            </div>
+          </label>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label className="flex-1">
+            <span> CVC</span>
+            <div className="mt-1 rounded border border-gray-300 p-3">
+              <CardCvcElement />
+            </div>
+          </label>
+        </div>
 
-      <button
+        {errorMessage && (
+          <div className="mb-4 font-semibold text-red-600">{errorMessage}</div>
+        )}
+        <section className="mx-auto flex max-w-4xl rounded-md bg-blue-100 p-4 text-blue-800">
+          <CircleAlert color="blue" size={30} />
+          <div className="pl-3">
+            <h4 className="font-semibold">Payment Protection</h4>
+            <p>
+              {" "}
+              Your payment is processes securely by Stripe. You&apos;ll be
+              charged when the service is completed
+            </p>
+          </div>
+        </section>
+      </section>
+      <StyledAsButton
         type="submit"
         disabled={!stripe || loading}
-        className="mt-2 w-full rounded-md bg-black p-5 font-bold text-white disabled:animate-pulse disabled:opacity-50"
-      >
-        {!loading ? `Pay $${amount}` : "Processing..."}
-      </button>
+        className="mx-auto my-10 flex w-full max-w-lg rounded-3xl bg-blue-500 p-4 font-bold text-white disabled:cursor-not-allowed disabled:opacity-50"
+        label={!loading ? `Confirm and Pay Bill` : "Processing..."}
+        // flex because it automatically comes with an inline-flex button, changing it to flex will allow it to be centered
+      />
     </form>
   );
 };
