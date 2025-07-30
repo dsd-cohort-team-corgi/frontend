ac89ebec0cd11948bfe70ba575df4d4fcc940e91
Janet Spellman <janetspellman13@gmail.com>
2025-07-30 15:50:46 -0700

    removed stripe logic, to focus on the information for this subissue

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,19 +2,10 @@
 
 import React from "react";
 import { useSearchParams } from "next/navigation";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import Checkout from "@/components/stripe/Checkout";
-import convertToSubcurrency from "@/utils/stripe/convertToSubcurrency";
+
 import BookingCheckoutPage from "@/components/stripe/BookingCheckoutPage";
 
-if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
-  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
-}
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
-
 export default function Page() {
-  const amount = 49.99;
   const searchParams = useSearchParams();
   const providerName = searchParams.get("providername") || "Green Thumb Pros";
   const date = searchParams.get("date") || "Monday, July 15th, 2025";
@@ -36,16 +27,6 @@ export default function Page() {
         serviceDuration={serviceDuration}
         serviceCost={serviceCost}
       />
-      <Elements
-        stripe={stripePromise}
-        options={{
-          mode: "payment",
-          amount: convertToSubcurrency(amount),
-          currency: "usd",
-        }}
-      >
-        <Checkout amount={convertToSubcurrency(Number(serviceCost))} />
-      </Elements>
     </div>
   );
 }
