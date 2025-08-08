91c6bed4367a3d230d314318bb2f371322bfa10e
Janet Spellman <janetspellman13@gmail.com>
2025-08-08 15:30:54 -0700

    refactoring: converting continue to booking button into a reusable component

---
+++ components/buttons/CheckoutButton.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import StyledAsButton from "@/components/StyledAsButton";
+import { useAuthContext } from "@/components/context-wrappers/AuthContext";
+import { useBooking } from "@/components/context-wrappers/BookingContext";
+import SignInModal from "@/components/SignInModal";
+
+export default function CheckoutButton() {
+  const { authContextObject } = useAuthContext();
+  const { booking, updateBooking } = useBooking();
+  const router = useRouter();
+
+  useEffect(() => {
+    updateBooking({
+      companyName: providerInfo?.company_name,
+      firstName: providerInfo?.first_name,
+      lastName: providerInfo?.last_name,
+      providerId: providerInfo?.id,
+      paymentIntentId: undefined,
+      // if they go from /checkout back to this page, perhaps to change the time or day we want them to keep all their selected information except for paymentIntentId
+    });
+  }, [providerInfo]);
+  function handleContinueToBooking() {
+    if (!booking.serviceId || !booking.time) return;
+
+    if (!authContextObject.supabaseUserId) {
+      signInOnOpen(); // show sign-in modal
+      return;
+    }
+
+    if (authContextObject.supabaseUserId) {
+      const isMissingProfileInfo =
+        !authContextObject.customerId ||
+        !authContextObject.streetAddress1 ||
+        !authContextObject.city ||
+        !authContextObject.state ||
+        !authContextObject.zip;
+
+      if (isMissingProfileInfo && !completeProfileIsOpen) {
+        openCompleteProfile();
+        return;
+      }
+    }
+    if (userSession) {
+      router.push(`/checkout`);
+    }
+  }
+
+  return (
+    <section>
+      <SignInModal isOpen={signInIsOpen} onOpenChange={signInOnOpen} />
+      <StyledAsButton
+        className="mb-4 mt-6 block w-11/12 px-0 disabled:bg-gray-500"
+        label="Continue to Booking"
+        onPress={() => handleContinueToBooking()}
+        disabled={!booking.serviceId || !booking.time || !booking.date}
+      />
+    </section>
+  );
+}
